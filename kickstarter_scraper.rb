6cc1ff33da675bb90d28b1df5f1d45835fdf760f
Catheryne Small <catheryn.small@gmail.com>
2020-06-13 21:15:08 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,7 +2,8 @@ require "nokogiri"
 require "pry"
 
 def create_project_hash
-  html = File.read('fixtures/kickstarter.html')
+  html = File.read('fixtures/kickstarter.html') 
+                    # projects: kickstarter.css("li.project.grid_4")
   kickstarter = Nokogiri::HTML(html)
   binding.pry
 end
